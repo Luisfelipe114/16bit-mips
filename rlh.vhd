b207156c8669f4fa52f784977ecc95e5a86e83ae
João Filipe <joaofilipeb045@gmail.com>
2022-10-14 10:00:57 -0300

    Corrigindo erros de sintaxe

--- rlh.vhd
+++ rlh.vhd
@@ -7,20 +7,22 @@ ENTITY rlh IS
         rlh_write : IN STD_LOGIC;
         rlh_reg : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
         data_in : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-        data_out : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+        data_out : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
     );
 END rlh;
 
 ARCHITECTURE Behavioral OF rlh IS
+    TYPE reg_array IS ARRAY (0 TO 0) OF STD_LOGIC_VECTOR (31 DOWNTO 0);
+    SIGNAL RLH : reg_array := (OTHERS => x"00000000");
 
 BEGIN
     PROCESS (clk_in)
     BEGIN
         IF rising_edge(clk_in) THEN
             IF rlh_write = '0' THEN
-                data_out <= rlh_reg;
+                data_out <= RLH(to_integer(unsigned(rlh_reg)));
             ELSE
-                rlh_reg <= data_in;
+                RLH(to_integer(unsigned(rlh_reg))) <= data_in;
             END IF;
         END IF;
     END PROCESS;
