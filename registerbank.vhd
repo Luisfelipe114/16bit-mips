14a01a78936c8e98e64de120097ef22cc7e2abf4
João Filipe <joaofilipeb045@gmail.com>
2022-10-13 21:10:20 -0300

    Apontamento do RD para os 4 registradores

--- registerbank.vhd
+++ registerbank.vhd
@@ -6,8 +6,8 @@ ENTITY registerfile IS
         reg_write : IN STD_LOGIC;
         write_reg : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
         write_data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-        RS_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-        RT_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        RF2_or_shift_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        RF_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
         RD_data_in : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
         data_out_1 : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
         data_out_2 : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
@@ -15,16 +15,18 @@ ENTITY registerfile IS
 END registerfile;
 
 ARCHITECTURE Behavioral OF registerfile IS
-    TYPE reg_array IS ARRAY(0 TO 15) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
-    SIGNAL REGISTER : reg_array := (OTHERS => x"0000");
+    TYPE reg_array IS ARRAY (0 TO 15) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
+    TYPE reg_rd_fields IS ARRAY (0 TO 3) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
+    SIGNAL REGISTER : reg_array := ("0000000000000000");
+    SIGNAL REGISTERS_RD : reg_rd_fields := ("0000");
 
 BEGIN
     PROCESS (clk_in)
     BEGIN
         IF rising_edge(clk_in) THEN
             IF reg_write = '0' THEN
-                data_out_1 <= REGISTER(to_integer(unsigned(RS_data_in)));
-                data_out_2 <= REGISTER(to_integer(unsigned(RT_data_in)));
+                data_out_1 <= REGISTER(to_integer(unsigned(RD_data_in)));
+                data_out_2 <= REGISTER(to_integer(unsigned(RF_data_in)));
             ELSE
                 REGISTER(to_integer(unsigned(write_reg))) <= write_data;
             END IF;
