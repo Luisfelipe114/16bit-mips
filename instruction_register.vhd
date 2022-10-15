a3328198442b47a3d1b169ca5b6932db49a43c2d
Victor <vhsouza606@gmail.com>
2022-10-15 11:48:19 -0300

    adicionando sinal alu_op ao IR

--- instruction_register.vhd
+++ instruction_register.vhd
@@ -9,6 +9,7 @@ ENTITY instruction_register IS
         instruction_in : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
         op_code_out : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
         intruction_op_out : OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
+        alu_op_out : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
         imm_data_out : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- 16 or 8
         j_imm_data_out : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
         sel_rF_out : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
@@ -25,6 +26,7 @@ BEGIN
 
             op_code_out <= reg_instruction(15 DOWNTO 10);
             instruction_op_out <= reg_instruction(13 DOWNTO 10);
+            alu_op_out <= reg_instruction(15 DOWNTO 14);
             sel_rD_out <= reg_instruction(9 DOWNTO 8);
             sel_rF_out <= reg_instruction(7 DOWNTO 4);
             sel_rF2_or_shift_out <= reg_instruction(3 DOWNTO 0);
