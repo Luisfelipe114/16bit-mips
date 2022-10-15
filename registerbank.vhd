e910ee0e09b0bf7e2cd77acfde1c4bdfc00bdfb7
João Filipe <joaofilipeb045@gmail.com>
2022-10-15 09:50:02 -0300

    Alterações no nome das variaveis e arrays

--- registerbank.vhd
+++ registerbank.vhd
@@ -7,27 +7,24 @@ ENTITY registerfile IS
         reg_write : IN STD_LOGIC;
         write_reg : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
         write_data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-        RF2_or_shift_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-        RF_data_in : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-        RD_data_in : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+        address_in_1 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        address_in_2 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
         data_out_1 : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
         data_out_2 : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
     );
 END registerfile;
 
 ARCHITECTURE Behavioral OF registerfile IS
-    TYPE reg_array IS ARRAY (0 TO 11) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
-    TYPE reg_rd_fields IS ARRAY (0 TO 3) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
+    TYPE reg_array IS ARRAY (0 TO 15) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
     SIGNAL REGISTERS : reg_array := (OTHERS => x"0000");
-    SIGNAL REGISTERS_RD : reg_rd_fields := (OTHERS => x"0000");
 
 BEGIN
     PROCESS (clk_in)
     BEGIN
         IF rising_edge(clk_in) THEN
             IF reg_write = '0' THEN
-                data_out_1 <= REGISTERS(to_integer(unsigned(RD_data_in)));
-                data_out_2 <= REGISTERS(to_integer(unsigned(RF_data_in)));
+                data_out_1 <= REGISTERS(to_integer(unsigned(address_in_1)));
+                data_out_2 <= REGISTERS(to_integer(unsigned(address_in_2)));
             ELSE
                 REGISTERS(to_integer(unsigned(write_reg))) <= write_data;
             END IF;
